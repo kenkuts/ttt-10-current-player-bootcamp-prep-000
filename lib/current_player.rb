c5818d7f455ca9cb60fd448b26389f929e7cbfda
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-01 04:28:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 def turn_count(board)
   turn = 0
+  board.each {|move| if move == "X" || move == "O" then turn += 1 end}
   
-  board.each {|move| if move == "X" || move == "O" then turn += 1}
+  return turn
 end
 
 def turn_player
